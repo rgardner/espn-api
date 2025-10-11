b1cf6797cd62ea50406d6320cc57d18ba02af780
Bob Gardner <bob.hn.gardner@gmail.com>
2025-10-11 16:27:34 -0400

    Export BoxScore class

--- espn_api/football/__init__.py
+++ espn_api/football/__init__.py
@@ -2,7 +2,8 @@ __all__ = ['League',
            'Team',
            'Matchup',
            'Player',
-           'BoxPlayer'
+           'BoxPlayer',
+           'BoxScore'
            ]
 
 from .league import League
@@ -10,3 +11,4 @@ from .team import Team
 from .matchup import Matchup
 from .player import Player
 from .box_player import BoxPlayer
+from .box_score import BoxScore
